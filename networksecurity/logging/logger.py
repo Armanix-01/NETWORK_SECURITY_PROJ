a09a4a0c6f1612b60f9775773172806525cd921b
Armanix-01 <armanixofficial01@gmail.com>
2025-12-26 08:05:51 +0530

    ETL pipeline completed

--- networksecurity/logging/logger.py
+++ networksecurity/logging/logger.py
@@ -4,12 +4,7 @@ import os
 from datetime import datetime
 
 LOG_FILE= f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
-##strftime (string format time) is a common function in
-#  programming (Python, C/C++, SQL, PHP) that converts 
-# date/time objects into human-readable strings, using 
-# special codes like %Y (year) or %m (month) to define 
-# the output format, offering precise control over how 
-# dates and times are displayed
+
 logs_path = os.path.join(os.getcwd(), "logs")
 os.makedirs(logs_path, exist_ok=True)
 LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
