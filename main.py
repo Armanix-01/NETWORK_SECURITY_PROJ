df57354a59d89762e8ec4d2efae1bd5d345df6c7
Armanix-01 <armanixofficial01@gmail.com>
2025-12-29 11:59:58 +0530

    Data Transformation Completed

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from networksecurity.components.data_ingestion import DataIngestion
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
-from networksecurity.entity.config_entity import DataIngestionConfig, TrainingPipelineConfig, DataValidationConfig
+from networksecurity.entity.config_entity import DataIngestionConfig, TrainingPipelineConfig, DataValidationConfig, DataTransformationConfig
 from networksecurity.components.data_validataion import DataValidation
 import sys
+from networksecurity.components.data_transformation import DataTransformation
 
 if __name__=="__main__":
     try:
@@ -19,7 +20,16 @@ if __name__=="__main__":
         logging.info("initiate the date ingestion")
         data_validation_artifact=data_validation.initiate_data_validation()
         logging.info("data validation completed")
-        print(data_validation_artifact)
+        logging.info("data transformation initiated")
+        data_transformation_config = DataTransformationConfig(training_pipeline_config=training_pipeline_config)
+        data_transformation = DataTransformation(
+            data_validataion_artifact= data_validation_artifact,
+            data_transformation_config=data_transformation_config
+        )
+        data_transformation_artifact = data_transformation.initiate_data_transformation()
+        logging.info("Data transformation completed")
+        print(data_transformation_artifact)
+
 
 
 
